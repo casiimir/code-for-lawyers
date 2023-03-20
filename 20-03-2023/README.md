cc151bf1c98072a200e631f88afbcb07556c50ab
casiimir <73138891+casiimir@users.noreply.github.com>
2023-03-20 18:18:42 +0100

    Lezione 20 Marzo 2023

---
+++ 20-03-2023/README.md
@@ -0,0 +1,11 @@
+# Materiale supplementare
+
+- https://it.wikipedia.org/wiki/Programmazione_(informatica)
+- https://it.wikipedia.org/wiki/Programmazione_procedurale
+- https://it.wikipedia.org/wiki/Programmazione_orientata_agli_oggetti
+- https://upload.wikimedia.org/wikipedia/commons/5/57/05arearettangolocodice.png
+
+# Python
+- Programma https://www.python.org/
+- Sintassi : https://s3-us-west-2.amazonaws.com/courses-images/wp-content/uploads/sites/3976/2019/01/15232501/Python-Cheat-Sheet-2.jpg
+- Approfondimenti: https://www.w3schools.com/python/python_intro.asp
---
+++ 20-03-2023/codice.py
@@ -0,0 +1,34 @@
+# SINTATSSI IF
+# if(2 == 3):
+#   print("2 è uguale 2")
+# elif(2 == 10):
+#   print("2 è uguale a 10")
+# else:
+#   print("Uguaglianza errata!")
+
+# TEMPERATURA
+# temperatura = 0
+
+# if(temperatura <= 15):
+#   print("Fa freddo!")
+# else:
+#   print("Si sta bene!")
+
+# SINTASSI WHILE
+# valore = 10
+
+# while(valore > 0):
+#   print(valore)
+#   valore = valore - 1
+
+temperatura = 20
+
+def generaTesto(nome):
+  print("Salve", nome)
+  print("La temperatura oggi è di", temperatura, "gradi")
+  print("Dimmi come posso esserti utile oggi")
+  print("-----------------------------")  
+
+generaTesto("Casi")
+generaTesto("Pippo")
+generaTesto("Prof")
